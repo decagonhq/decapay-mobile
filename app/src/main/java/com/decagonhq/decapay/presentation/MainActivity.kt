fac9da991b2917fe54535e4a1207b112e20e1708
Osehi <osehiaseehilen@gmail.com>
2022-07-19 13:14:33 +0100

    Authentication navigation graph added to handle auth navigation

--- app/src/main/java/com/decagonhq/decapay/presentation/MainActivity.kt
+++ app/src/main/java/com/decagonhq/decapay/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.decagonhq.decapay
+package com.decagonhq.decapay.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.decapay.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
