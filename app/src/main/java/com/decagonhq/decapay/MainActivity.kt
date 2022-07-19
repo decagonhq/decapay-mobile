5f63499be7908241255d6a4c5159d862220c643e
Osehi <osehiaseehilen@gmail.com>
2022-07-19 12:38:11 +0100

    Test the Login fragment

--- app/src/main/java/com/decagonhq/decapay/MainActivity.kt
+++ app/src/main/java/com/decagonhq/decapay/MainActivity.kt
@@ -2,10 +2,19 @@ package com.decagonhq.decapay
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.decapay.feature.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * add views and variables
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val manageFragment = supportFragmentManager
+        val transaction = manageFragment.beginTransaction()
+        transaction.add(R.id.main_activity_view_login_fragment, LoginFragment())
+        transaction.commit()
     }
 }
