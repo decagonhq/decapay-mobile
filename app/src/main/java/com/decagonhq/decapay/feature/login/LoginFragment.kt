dd7c7f87c257252cb5042d19673ee53dc4fe8df4
Osehi <osehiaseehilen@gmail.com>
2022-07-20 13:10:38 +0100

    Added TextChangedListener to the email field to give validation error

--- app/src/main/java/com/decagonhq/decapay/feature/login/LoginFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/login/LoginFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.decagonhq.decapay.common.utils.validation.inputfieldvalidation.LoginInputValidation
 import com.decagonhq.decapay.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -12,6 +14,7 @@ class LoginFragment : Fragment() {
      * declare views and variables
      */
     private val TAG = "LOGINFRAGMENT"
+    private lateinit var receivedEmail: String
     private var _binding: FragmentLoginBinding? = null
     val binding get() = _binding!!
 
@@ -33,10 +36,28 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentLoginBinding.bind(view)
+
+        // capture the input email
+        binding.loginFragmentEmailTextinputedittextEmailTiedt.addTextChangedListener {
+            receivedEmail = binding.loginFragmentEmailTextinputedittextEmailTiedt.text.toString()
+            onEmailTextChanged(receivedEmail)
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+    private fun onEmailTextChanged(receivedEmail: String){
+        if (LoginInputValidation.validateEmailForTextWatcher(receivedEmail) == "Field cannot be empty"){
+            binding.loginFragmentEmailTextinputlayoutEmailTil.error = "Field cannot be empty"
+        } else if (LoginInputValidation.validateEmailForTextWatcher(receivedEmail) == "Invalid email"){
+            binding.loginFragmentEmailTextinputlayoutEmailTil.error = "Invalid email"
+        } else {
+            binding.loginFragmentEmailTextinputlayoutEmailTil.error = ""
+        }
+    }
+
+
 }
