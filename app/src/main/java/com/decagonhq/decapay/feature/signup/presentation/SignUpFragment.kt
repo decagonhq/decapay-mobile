426e442dad7fd6e6e98729abe4e903593552c597
omudavid <omudavid@gmail.com>
2022-07-26 10:40:54 +0100

    Combine

--- app/src/main/java/com/decagonhq/decapay/feature/signup/presentation/SignUpFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/signup/presentation/SignUpFragment.kt
@@ -148,7 +148,6 @@ class SignUpFragment : Fragment() {
                         is Resource.Loading -> {
 
                         }
-                        else -> {}
                     }
                 }
             }
