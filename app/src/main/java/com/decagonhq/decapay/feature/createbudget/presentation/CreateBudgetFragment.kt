aabf2b6753a8d863e7b6d6bb0be2951b68c6b845
Osehi <osehiaseehilen@gmail.com>
2022-08-03 11:15:50 +0100

    Added done button

--- app/src/main/java/com/decagonhq/decapay/feature/createbudget/presentation/CreateBudgetFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/createbudget/presentation/CreateBudgetFragment.kt
@@ -6,11 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.decagonhq.decapay.R
 import com.decagonhq.decapay.databinding.FragmentCreateBudgetBinding
 import com.google.android.material.snackbar.Snackbar
-
 
 class CreateBudgetFragment : Fragment() {
     /**
@@ -47,7 +44,7 @@ class CreateBudgetFragment : Fragment() {
             ).show()
         }
         // put a click listerner on the budget period
-        binding.createBudgetFragmentBudgetPeriodTil.setEndIconOnClickListener {
+        binding.createBudgetFragmentBudgetPeriodTv.setOnClickListener {
             Toast.makeText(
                 requireContext(),
                 "I am clicked",
