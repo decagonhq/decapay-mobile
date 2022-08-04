de1c55e1e58ad0d9eaf0ff3e014c80917b48c6ac
omudavid <omudavid@gmail.com>
2022-08-04 13:18:33 +0100

    Commit before checkout

---
+++ app/src/main/java/com/decagonhq/decapay/feature/budgetdetails/presentation/BudgetDetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.decagonhq.decapay.feature.budgetdetails.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.decagonhq.decapay.R
+import com.decagonhq.decapay.databinding.FragmentBudgetDetailsBinding
+import com.decagonhq.decapay.databinding.FragmentBudgetListBinding
+import com.decagonhq.decapay.feature.listbudget.adapter.BudgetListAdapter
+
+class BudgetDetailsFragment : Fragment() {
+
+    private var _binding: FragmentBudgetDetailsBinding? = null
+    val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBudgetDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
