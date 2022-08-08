fafc505bcd102f93637295ade3658b79ddc928c3
Osehi <osehiaseehilen@gmail.com>
2022-08-08 12:59:11 +0100

    Added a list of years

---
+++ app/src/main/java/com/decagonhq/decapay/feature/createbudget/data/staticdata/YearList.kt
@@ -0,0 +1,20 @@
+package com.decagonhq.decapay.feature.createbudget.data.staticdata
+
+object YearList {
+    val yearList: ArrayList<String>
+        get() {
+            val yearListData = ArrayList<String>()
+            yearListData.add("2022")
+            yearListData.add("2023")
+            yearListData.add("2024")
+            yearListData.add("2025")
+            yearListData.add("2026")
+            yearListData.add("2027")
+            yearListData.add("2028")
+            yearListData.add("2029")
+            yearListData.add("2030")
+            yearListData.add("2031")
+
+            return yearListData
+        }
+}
