c5d737bbe5ef288fa8bb57ed698d7c286ff4d3e6
Osehi <osehiaseehilen@gmail.com>
2022-08-08 10:08:13 +0100

    Lint formeted code

--- app/src/main/java/com/decagonhq/decapay/feature/createbudget/data/staticdata/CalendarMonth.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/createbudget/data/staticdata/CalendarMonth.kt
@@ -2,21 +2,21 @@ package com.decagonhq.decapay.feature.createbudget.data.staticdata
 
 object CalendarMonth {
     val calendarMonth: ArrayList<String>
-    get() {
-        val calendarMonthData = ArrayList<String>()
-        calendarMonth.add("January")
-        calendarMonth.add("February")
-        calendarMonth.add("March")
-        calendarMonth.add("April")
-        calendarMonth.add("May")
-        calendarMonth.add("June")
-        calendarMonth.add("July")
-        calendarMonth.add("August")
-        calendarMonth.add("September")
-        calendarMonth.add("October")
-        calendarMonth.add("November")
-        calendarMonth.add("December")
+        get() {
+            val calendarMonthData = ArrayList<String>()
+            calendarMonth.add("January")
+            calendarMonth.add("February")
+            calendarMonth.add("March")
+            calendarMonth.add("April")
+            calendarMonth.add("May")
+            calendarMonth.add("June")
+            calendarMonth.add("July")
+            calendarMonth.add("August")
+            calendarMonth.add("September")
+            calendarMonth.add("October")
+            calendarMonth.add("November")
+            calendarMonth.add("December")
 
-        return calendarMonthData
-    }
+            return calendarMonthData
+        }
 }
--- app/src/main/java/com/decagonhq/decapay/feature/createbudget/data/staticdata/WeekOfTheMonth.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/createbudget/data/staticdata/WeekOfTheMonth.kt
@@ -2,14 +2,14 @@ package com.decagonhq.decapay.feature.createbudget.data.staticdata
 
 object WeekOfTheMonth {
     val weekOfTheMonth: ArrayList<String>
-    get() {
-        val weekOfTheMonthData = ArrayList<String>()
-        weekOfTheMonthData.add("Week 1")
-        weekOfTheMonthData.add("Week 2")
-        weekOfTheMonthData.add("Week 3")
-        weekOfTheMonthData.add("Week 4")
-        weekOfTheMonthData.add("Week 5")
+        get() {
+            val weekOfTheMonthData = ArrayList<String>()
+            weekOfTheMonthData.add("Week 1")
+            weekOfTheMonthData.add("Week 2")
+            weekOfTheMonthData.add("Week 3")
+            weekOfTheMonthData.add("Week 4")
+            weekOfTheMonthData.add("Week 5")
 
-        return weekOfTheMonthData
-    }
+            return weekOfTheMonthData
+        }
 }
