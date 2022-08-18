e47ad55b04f5978bb4ed19b57ffda66a8bd0a673
Osehi <osehiaseehilen@gmail.com>
2022-08-18 10:47:21 +0100

    Added observer

--- app/src/main/java/com/decagonhq/decapay/feature/createbudgetcategory/presentation/CreateBudgetCategoryFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/createbudgetcategory/presentation/CreateBudgetCategoryFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.decagonhq.decapay.common.utils.resource.Resource
+import com.decagonhq.decapay.common.utils.uihelpers.showPleaseWaitAlertDialog
 import com.decagonhq.decapay.common.utils.validation.inputfieldvalidation.CreateBudgetCategoryInputValidation
 import com.decagonhq.decapay.databinding.FragmentCreateBudgetCategoryBinding
+import com.decagonhq.decapay.feature.createbudgetcategory.data.network.model.CreateBudgetCategoryRequestBody
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CreateBudgetCategoryFragment : Fragment() {
@@ -18,6 +27,8 @@ class CreateBudgetCategoryFragment : Fragment() {
     private val TAG = "CREATEBUDGETFRAG"
     private var _binding: FragmentCreateBudgetCategoryBinding? = null
     val binding: FragmentCreateBudgetCategoryBinding get() = _binding!!
+    private var pleaseWaitDialog: AlertDialog? = null
+    private val creatBudgetCategoryViewModel: CreateBudgetCategoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +48,7 @@ class CreateBudgetCategoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentCreateBudgetCategoryBinding.bind(view)
+        pleaseWaitDialog = showPleaseWaitAlertDialog()
         // on click on the add button
         binding.createBudgetCategoryFragmentNameCategoryAddButtonBtn.setOnClickListener {
             // receive the value from the input field
@@ -50,6 +62,41 @@ class CreateBudgetCategoryFragment : Fragment() {
                 ).show()
             } else {
                 // make the network call
+                creatBudgetCategoryViewModel.userCreateBudgetCategory(
+                    CreateBudgetCategoryRequestBody(categoryName)
+                )
+                pleaseWaitDialog?.let { it.show() }
+            }
+        }
+        initObserver()
+    }
+
+    private fun initObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                creatBudgetCategoryViewModel.createBudgetCategoryCaptureResponse.collect {
+                    when(it) {
+                        is Resource.Success -> {
+                            pleaseWaitDialog?.let { it.dismiss() }
+                            Snackbar.make(
+                                binding.root,
+                                "${it.data.message}",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        is Resource.Error -> {
+                            pleaseWaitDialog?.let { it.dismiss() }
+                            Snackbar.make(
+                                binding.root,
+                                "${it.message}",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        is Resource.Loading -> {
+
+                        }
+                    }
+                }
             }
         }
     }
