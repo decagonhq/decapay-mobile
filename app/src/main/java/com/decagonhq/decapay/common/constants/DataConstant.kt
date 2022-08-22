ca3a774f29e06470dc25e011e8dd127cbb74e510
Osehi <osehiaseehilen@gmail.com>
2022-08-22 17:02:16 +0100

    Edit constant

--- app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
+++ app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
@@ -5,6 +5,5 @@ object DataConstant {
     const val SET_BUDGET_ID = "set_budget_id"
     const val BUDGET_ITEM = "BUDGET_ITEM"
     const val BUDGET_ID_BOTTOMSHEET = "BUDGET_ID_BOTTOMSHEET"
-    const val BUDGET_ITEM_BOTTOMSHEET = "BUDGET_ITEM_BOTTOMSHEET"
     const val BUDGET_CATEGORY_LIST_TITLE = "budget_category_list_title"
 }
