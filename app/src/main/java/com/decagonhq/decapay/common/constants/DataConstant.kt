f278b750b8dabcfed10e53ac1712d841a0700d5a
Osehi <osehiaseehilen@gmail.com>
2022-08-22 19:42:41 +0100

    Refactor DataConstant

--- app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
+++ app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
@@ -3,7 +3,5 @@ package com.decagonhq.decapay.common.constants
 object DataConstant {
     const val BUDGET_ID = "BUDGET_ID"
     const val BUDGET_ITEM = "BUDGET_ITEM"
-    const val BUDGET_ID_BOTTOMSHEET = "BUDGET_ID_BOTTOMSHEET"
-    const val BUDGET_CATEGORY_LIST_TITLE = "budget_category_list_title"
     const val BUDGET_CATEGORY_LIST_ITEM = "budget_category_list_item"
 }
