8f45dd4f0a5b240e8400e3e020329f95d01e56fb
Osehi <osehiaseehilen@gmail.com>
2022-08-22 15:49:16 +0100

    Added new constants

--- app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
+++ app/src/main/java/com/decagonhq/decapay/common/constants/DataConstant.kt
@@ -2,6 +2,7 @@ package com.decagonhq.decapay.common.constants
 
 object DataConstant {
     const val BUDGET_ID = "BUDGET_ID"
+    const val SET_BUDGET_ID = "set_budget_id"
     const val BUDGET_ITEM = "BUDGET_ITEM"
     const val BUDGET_ID_BOTTOMSHEET = "BUDGET_ID_BOTTOMSHEET"
     const val BUDGET_ITEM_BOTTOMSHEET = "BUDGET_ITEM_BOTTOMSHEET"
