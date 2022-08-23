715edd0cecfab32ff2842a1b394dc33882572ab8
Osehi <osehiaseehilen@gmail.com>
2022-08-23 13:34:21 +0100

    Removed spinner logic

--- app/src/main/java/com/decagonhq/decapay/feature/editbudgetlineitem/presentation/EditBudgetLineItemBottomSheetFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/editbudgetlineitem/presentation/EditBudgetLineItemBottomSheetFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import com.decagonhq.decapay.R
 import com.decagonhq.decapay.common.constants.DataConstant
 import com.decagonhq.decapay.common.utils.resource.Resource
 import com.decagonhq.decapay.databinding.FragmentEditBudgetLineItemBinding
@@ -56,8 +53,7 @@ class EditBudgetLineItemBottomSheetFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         // get category list
         getBudgetCategoryListViewModel.getBudgetCategoryList()
-        // disable the spinner from selection
-        binding.editBudgetLineItemBottomSheetFragmentCategorySpinner.isEnabled = false
+
         // set the projected amount to the editable text input fields
         binding.editBudgetLineItemBottomSheetFragmentAmountTiedt.setText(projectedAnount.toString())
 
@@ -74,32 +70,6 @@ class EditBudgetLineItemBottomSheetFragment : BottomSheetDialogFragment() {
                 getBudgetCategoryListViewModel.getBudgetCategoryListResponse.collect {
                     when (it) {
                         is Resource.Success -> {
-                            val budgetCategoryList = ArrayList<String>()
-                            val categories = it.data.data
-                            if (categories != null) {
-                                for (item in categories) {
-                                    if (item != null) {
-                                        item.title?.let { categoryTitle -> budgetCategoryList.add(categoryTitle) }
-                                    }
-                                }
-                            }
-                            val budgetCategoryAdapter = ArrayAdapter<String>(requireContext(), R.layout.list_item, budgetCategoryList)
-                            binding.editBudgetLineItemBottomSheetFragmentCategorySpinner.adapter = budgetCategoryAdapter
-                            binding.editBudgetLineItemBottomSheetFragmentCategorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                                override fun onItemSelected(
-                                    parent: AdapterView<*>?,
-                                    view: View?,
-                                    position: Int,
-                                    id: Long
-                                ) {
-                                    //
-                                }
-
-                                override fun onNothingSelected(p0: AdapterView<*>?) {
-                                    //
-                                }
-                            }
-                            binding.editBudgetLineItemBottomSheetFragmentCategorySpinner.setSelection(budgetCategoryList.indexOf(selectedCategory))
                         }
                         is Resource.Error -> {
                         }
