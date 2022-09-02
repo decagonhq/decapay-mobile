8f29007bdbf65c678c38341065d2bd40235fadaf
Osehi <osehiaseehilen@gmail.com>
2022-09-02 16:56:36 +0100

    Removed logged values

--- app/src/main/java/com/decagonhq/decapay/feature/budgetdetails/presentation/BudgetDetailsFragment.kt
+++ app/src/main/java/com/decagonhq/decapay/feature/budgetdetails/presentation/BudgetDetailsFragment.kt
@@ -276,14 +276,12 @@ class BudgetDetailsFragment : Fragment(), LineItemClicker {
         val bundle = Bundle()
         bundle.putSerializable(DataConstant.LOG_EXPENSE_BUDGET_LINE_ITEM_SELECTED, currentLineItem)
         bundle.putString(DataConstant.LOG_EXPENSE_SELECTED_DATE, calendarSelectedDate)
-        Log.d(TAG, "selected date on calendar: $calendarSelectedDate")
         findNavController().navigate(R.id.logExpenseBottomSheetFragment, bundle)
     }
 
     override fun onClickItem(currentLineItem: LineItem, position: Int, view: View) {
         val bundle = Bundle()
 
-        //  budgetId?.let { bundle.putInt(DataConstant.BUDGET_ID, it) }
         bundle.putInt(DataConstant.BUDGET_ID, budgetId!!)
         bundle.putString(DataConstant.CATEGORY, currentLineItem.category)
         bundle.putInt(DataConstant.CATEGORY_ID, currentLineItem.categoryId)
