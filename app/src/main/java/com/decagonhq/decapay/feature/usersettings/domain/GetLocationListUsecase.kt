ce72584ab60e9f18415bd9f0f2d39334eb51581f
Osehi <osehiaseehilen@gmail.com>
2022-09-07 07:08:53 +0100

    GetLocationListViewModel added

---
+++ app/src/main/java/com/decagonhq/decapay/feature/usersettings/domain/GetLocationListUsecase.kt
@@ -0,0 +1,29 @@
+package com.decagonhq.decapay.feature.usersettings.domain
+
+import com.decagonhq.decapay.common.utils.errorhelper.ExceptionHandler
+import com.decagonhq.decapay.common.utils.resource.Resource
+import com.decagonhq.decapay.feature.usersettings.data.network.model.GetLocationResponseDemo
+import com.decagonhq.decapay.feature.usersettings.domain.repository.GetLocationRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class GetLocationListUsecase @Inject constructor(
+    private val getLocationRepository: GetLocationRepository,
+    private val exceptionHandler: ExceptionHandler
+) {
+
+    operator fun invoke(): Flow<Resource<GetLocationResponseDemo>> = flow {
+        try {
+            emit(Resource.Loading())
+            val response = getLocationRepository.getLocationList()
+            emit(Resource.Success(response))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server check your internet connection"))
+        } catch (e: Exception) {
+            val message = exceptionHandler.parse(e)
+            emit(Resource.Error(message))
+        }
+    }
+}
