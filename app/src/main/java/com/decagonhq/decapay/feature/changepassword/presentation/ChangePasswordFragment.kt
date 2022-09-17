8b0b3e30cf02e8ebaf98640a0ba08ab1aabc23b2
Osehi <osehiaseehilen@gmail.com>
2022-09-17 23:48:55 +0100

    Added changePasswordLayout

---
+++ app/src/main/java/com/decagonhq/decapay/feature/changepassword/presentation/ChangePasswordFragment.kt
@@ -0,0 +1,24 @@
+package com.decagonhq.decapay.feature.changepassword.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.decagonhq.decapay.R
+
+class ChangePasswordFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+}
