d181d311e9f0ed8ab70938a0bee20f3143b7f4bc
Osehi <osehiaseehilen@gmail.com>
2022-09-18 23:48:06 +0100

    Refactored lint

--- app/src/androidTest/java/com/decagonhq/decapay/feature/listbudget/BudgetListFragmentTest.kt
+++ app/src/androidTest/java/com/decagonhq/decapay/feature/listbudget/BudgetListFragmentTest.kt
@@ -1,12 +1,10 @@
 package com.decagonhq.decapay.feature.listbudget
 
-import androidx.core.os.bundleOf
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.decagonhq.decapay.R
-import com.decagonhq.decapay.common.data.model.Content
 import com.decagonhq.decapay.feature.listbudget.presentation.BudgetListFragment
 import com.decagonhq.decapay.fragmenttestutils.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -17,26 +15,21 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class BudgetListFragmentTest {
 
-
     private val email = "omudavid@gmail.com"
     private val password = "Qwerty12"
 
-
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
-
 
     @Before
     fun setUp() {
         hiltRule.inject()
     }
-
 
     @Test
     fun screen_renders() {
@@ -44,9 +37,5 @@ class BudgetListFragmentTest {
         val scenario = launchFragmentInHiltContainer<BudgetListFragment>()
         Espresso.onView(ViewMatchers.withId(R.id.budgetListFragment_create_budget_fab))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
-
-
-
     }
 }
