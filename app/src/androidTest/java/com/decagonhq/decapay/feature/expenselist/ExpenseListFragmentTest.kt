53ffcea6530891f56acfb25657bc8ae32c8e8f4d
omudavid <omudavid@gmail.com>
2022-09-19 11:12:36 +0100

    Commit before checkout

--- app/src/androidTest/java/com/decagonhq/decapay/feature/expenselist/ExpenseListFragmentTest.kt
+++ app/src/androidTest/java/com/decagonhq/decapay/feature/expenselist/ExpenseListFragmentTest.kt
@@ -1,8 +1,10 @@
 package com.decagonhq.decapay.feature.expenselist
 
 import androidx.core.os.bundleOf
+import androidx.navigation.NavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.decagonhq.decapay.common.constants.DataConstant
+import com.decagonhq.decapay.feature.budgetdetails.data.network.model.bundle.LogExpenseData
 import com.decagonhq.decapay.feature.expenseslist.presentation.ExpensesListFragment
 import com.decagonhq.decapay.fragmenttestutils.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -12,6 +14,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
@@ -28,7 +31,12 @@ class ExpenseListFragmentTest {
 
     @Test
     fun screen_renders() {
-        val fragmentArgs = bundleOf(DataConstant.BUDGET_ID to 1, DataConstant.CATEGORY_ID to 1, DataConstant.CATEGORY to "Transportation")
+        val mockNavController = mock(NavController::class.java)
+        val fragmentArgs = bundleOf(DataConstant.BUDGET_ID to 1, DataConstant.CATEGORY_ID to 1,
+            DataConstant.CATEGORY to "Transportation",DataConstant.LOG_EXPENSE_DATA
+                    to LogExpenseData(null,null,null,
+                null,null,null)
+        )
         val scenario = launchFragmentInHiltContainer<ExpensesListFragment>(fragmentArgs)
     }
 }
